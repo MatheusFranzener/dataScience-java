035230b22898ce062b68108fc3c5c16c1b2b02fa
Matheus Franzener Hohmann <matheus_hohmann@estudante.sc.senai.br>
2023-06-20 21:48:28 -0300

    feat: apache spark java

---
+++ data-science-java/src/main/java/br/sc/senai/datasciencejava/DataScienceJavaApplication.java
@@ -0,0 +1,13 @@
+package br.sc.senai.datasciencejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataScienceJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataScienceJavaApplication.class, args);
+	}
+
+}
---
+++ data-science-java/src/test/java/br/sc/senai/datasciencejava/DataScienceJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.sc.senai.datasciencejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataScienceJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
